283622f339a9f3a559371b92095868d891220a0e
Robson Thiago <56169778+r0b14@users.noreply.github.com>
2022-11-29 21:30:04 +0000

    escrevendo todas as questoes

--- JS/05-praticando-avancando/03-exericicio.js
+++ JS/05-praticando-avancando/03-exericicio.js
@@ -39,4 +39,27 @@
       isSubscribed: true
     };
 
-    console.log(`${student.name} de idade ${student.age} e pesa ${weight}`);
+    console.log(`${student.name} de idade ${student.age} e pesa ${weight} kg`);
+
+/**
+    5. Declare uma variável do tipo Array, de nome students e atribua a ela nenhum valor, ou seja, somente o Array vazio.
+ */
+
+/**
+    6. Reatribua valor para a variavel acim, colocando dentro dela o objeto student da questão 4. (Não copiar e colar o objeto, mas usar o objeto criado e inserir ele no Array)
+**/
+
+/**
+    7. Coloque no console o valor da posição zero do Array acima
+**/
+
+/**
+    8. Crie um novo student e coloque na posição 1 do array students
+ */
+
+/**
+    9. Sem rodar o código responda qual é a resposat do codigo abaixo e por que? Após sua resposta, rode o código e veja se você acertou.
+
+    Console.log(a)
+    var a = 1
+ */
