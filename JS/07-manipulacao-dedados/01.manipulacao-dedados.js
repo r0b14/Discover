6dc01771b81a332bb30c017b6b70be06495451e9
Robson-ubt <56169778+r0b14@users.noreply.github.com>
2022-12-12 10:12:05 -0300

    em estudo

--- JS/07-manipulacao-dedados/01.manipulacao-dedados.js
+++ JS/07-manipulacao-dedados/01.manipulacao-dedados.js
@@ -34,7 +34,7 @@ console.log(String(number).length);
  * Trocando ponto por virgula.
  **/
 
-let number1 = 345.1155 // Inserindo 2 casas décimais para o número
+let number1 = 345.1155; // Inserindo 2 casas décimais para o número
 console.log(number1.toFixed(2)); // limitando as casas decimais do número
 console.log(number1.toFixed(2).replace(".", ",")); // trocando de ponto para virgula
 
@@ -46,3 +46,31 @@ let keyWords = "Por valor, contratual IC - SEE";
 console.log(keyWords.toUpperCase()); // Tornando tudo maiusculas
 console.log(keyWords.toLowerCase()); // Tornando tudo minusculas
 console.log(keyWords.toUpperCase().toLowerCase); // mostrando que podemos usar +1 metodo
+
+/**
+ * Manipulando strings e arrays
+ **/
+
+// Separar um texto que contém espaços, em um novo array onde cada texto é uma posição do array. Depois disso, transformar o array em um texto e onde eram espaços, colocar `_`
+
+let text1 = "Texto inserido pelo usuario";
+let arrayText1 = text1.split(" "); //o argumento do método é base que permite separar as palavras da frase/texto. Nesse caso vai ser separado todas as vezes que encontrar um espaços
+console.log(arrayText1); // Percebe-se que ele "come" no momento que encontra o elemento informado.
+
+let textoConverted = arrayText1.join("_"); // Converteu todo o texto que estava inserido em um array em uma string e separou as palavras com o `_`
+console.log(textoConverted); // mostra na tela a junção
+
+/**
+ * Encontrando palavras em frases
+ **/
+
+let frase1 = "O amor sempre mora na vida"
+console.log(frase1.includes("amor")); // true
+
+/**
+ * Criando array com construtor
+ **/
+
+let myArray1 = [1, 2, 3, 4, 5] // essa seria o método convencional
+let myArray2 = new Array('a', 'b', 'c', 'd')// ou criar com espaços vazios = `new Array(10)`
+console.log(myArray2); // mostrando
